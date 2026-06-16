6a88f5fe0610fb9f6e3c60624bf85870b1a1f0b9
mjulisarchive <mjulis.archive@gmail.com>
2026-06-16 10:51:27 +0900

    Update credits.ts

--- data/credits.ts
+++ data/credits.ts
@@ -18,7 +18,7 @@ export const members: Member[] = [
   },
   {
     name: '권예슬',
-    studentId: '6023',
+    studentId: '60230295',
     role: '사이트 개발 및 콘텐츠 관리',
     year: '2026 ~ present',
     email: 'yxsxux@mju.ac.kr'
